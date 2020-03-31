a36d7f01a849f1591c0f36d7729aceeec8f28834
Robert Osborne <robertosborne@Roberts-Mac-mini.local>
2020-03-30 22:07:45 -0700

    Add A11y

--- src/lib/Elements/Button/Button.stories.js
+++ src/lib/Elements/Button/Button.stories.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
+import { withA11y } from '@storybook/addon-a11y'
 import { Button } from './Button'
 
 export default {
-    title: 'Button',
-    component: Button
+    component: Button,
+    decorators: [withA11y],
+    title: 'Button'
 }
 
 export const TestButton = () => (
     <Button buttonText="Test Button" onClick={action('clicked')} />
+)
+
+export const inaccessible = () => (
+    <button style={{ backgroundColor: 'red', color: 'darkRed' }}>
+        Inaccessible button
+    </button>
 )
