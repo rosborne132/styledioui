e74885a85048b3c4cfe89e08ed899e43170812a3
Robert Osborne <robertosborne@Roberts-Mac-mini.local>
2020-03-31 21:22:24 -0700

    Add react testing library

---
+++ src/lib/Elements/Buttons/Button.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { Button } from './Button'
+
+describe('<Button />', () => {
+    test('', () => {
+        const { debug } = render(
+            <Button
+                buttonText="Test Button"
+                isLoading={false}
+                onClick={() => console.log('This is a test')}
+            />
+        )
+        debug()
+    })
+})
