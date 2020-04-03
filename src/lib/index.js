3bba54849a21b6b5de018fd5e898b0ce8f0a1286
rosborne132 <ozborne132@gmail.com>
2020-04-02 20:43:12 -0700

    Update components

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,12 @@
-import { Button } from './Elements/Buttons/Button'
-import { Icons } from './Elements/Icons/Icons'
+import * as styles from './shared/styles'
+import * as global from './shared/global'
+import * as animation from './shared/animation'
+import * as icons from './shared/icons'
 
-export { Button, Icons }
+export { styles, global, animation, icons }
+
+export * from './Avatar'
+export * from './Badge'
+export * from './Button'
+export * from './Icon'
+export * from './Link'
