abedccddabfae4f5415b708599615b838db90713
rosborne132 <ozborne132@gmail.com>
2020-04-04 18:17:12 -0700

    Refactor Icon component and backfill tests

--- src/lib/Elements/Icon/Icon.js
+++ src/lib/Elements/Icon/Icon.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+
 import { icons } from '../../shared/icons'
+import { typography } from '../../shared/styles'
 
 const Svg = styled.svg`
     display: ${props => (props.block ? 'block' : 'inline-block')};
@@ -14,32 +16,30 @@ const Path = styled.path`
     fill: currentColor;
 `
 
-/**
- * An Icon is a piece of visual element, but we must ensure its accessibility while using it.
- * It can have 2 purposes:
- *
- * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
- * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
- */
-export function Icon({ icon, block, ...props }) {
-    return (
-        <Svg
-            viewBox="0 0 1024 1024"
-            width="20px"
-            height="20px"
-            block={block}
-            {...props}
-        >
-            <Path d={icons[icon]} />
-        </Svg>
-    )
-}
+export const Icon = ({ block = false, icon, size = 'm1', ...props }) => (
+    <Svg
+        data-testid="icon"
+        viewBox="0 0 1024 1024"
+        width={typography.size[size]}
+        height={typography.size[size]}
+        block={block}
+        {...props}
+    >
+        <Path d={icons[icon]} />
+    </Svg>
+)
 
 Icon.propTypes = {
+    /**
+        Boolean value that determines the icon block style.
+    */
+    block: PropTypes.bool,
+    /**
+        String that determine the icon displayed.
+    */
     icon: PropTypes.string.isRequired,
-    block: PropTypes.bool
-}
-
-Icon.defaultProps = {
-    block: false
+    /**
+        Determines the size of the icon.
+    */
+    size: PropTypes.oneOf(['s3', 'm2', 'm3', 'l1', 'l2', 'l3'])
 }
